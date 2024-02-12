e86e6595d245efa9a32e2813df889848f221abab
renz <johnlosabio10@gmail.com>
2024-02-12 17:17:18 +0800

    gg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "test", name, email, message }),
+      body: encode({ "form-name": "contact", name, email, message }),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
