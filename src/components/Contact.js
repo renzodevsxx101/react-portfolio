6e8e7691380119d2a69194d4396943825ba06809
renz <johnlosabio10@gmail.com>
2024-02-12 17:23:05 +0800

    G

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -72,7 +72,7 @@ export default function Contact() {
         </div>
         <form
           netlify
-          name="test"
+          name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
