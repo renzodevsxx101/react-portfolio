ec2216d24f392d138fa6f5a0f80cb30d3f8ee5da
renz <johnlosabio10@gmail.com>
2024-02-12 15:43:33 +0800

    gg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -69,10 +69,9 @@ export default function Contact() {
           netlify="true"
           name="contact"
           method="POST"
-          onSubmit={handleSubmit}
+          // onSubmit={handleSubmit}
           data-netlify-honeypot="bot-field" // Optional: For Netlify's spam filtering
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-          action="/"
         >
           {/* Hidden input for Netlify to identify this form */}
           <input type="hidden" name="form-name" value="contact" />
