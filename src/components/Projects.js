72292d65382d8867f322af07939c364cc9e88278
CPI-JLPAGDANGANAN <johnlosabio10@gmail.com>
2024-09-21 19:49:18 +0800

    Added Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,31 +24,38 @@ export default function Projects() {
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4"
-            >
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed text-white">{project.description}</p>
-                </div>
-              </div>
-            </a>
-          ))}
+  {projects.map((project) => (
+    <a
+      href={project.link}
+      key={project.image}
+      className="sm:w-1/2 w-100 p-4"
+    >
+      <div className="flex relative">
+        <img
+          alt="gallery"
+          className="absolute inset-0 w-100 h-64 object-cover object-center"
+          src={project.image}
+        />
+        <div className="px-8 py-10 h-64 relative z-10 w-100 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+          <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+            {project.subtitle}
+          </h2>
+          <h1 className="title-font text-lg font-medium text-white mb-3">
+            {project.title}
+          </h1>
+          <p className="leading-relaxed text-white">{project.description}</p>
+          <a
+            href={project.link}
+            className="mt-4 inline-block px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          >
+            Preview
+          </a>
         </div>
+      </div>
+    </a>
+  ))}
+</div>
+
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -1,7 +1,38 @@
 export const projects = [
   {
+    title: "AIA Website",
+    subtitle: "React, SCSS, API Integration",
+    description:
+      "Created various mobile responsive web pages and new features using React, SCSS, Git and Bitbucket.",
+    image: "./aia.gif",
+    link: "https://wwwsit.aia.com.ph/en/myaia/service/service.html/en/myaia/en/myaia/home.html",
+  },  {
+    title: "Sports Gallery",
+    subtitle: "HTML5, CSS3, JavaScript",
+    description:
+      "Created a mobile responsive library of youtube thumbnails using HTML, CSS, and JavaScript.",
+    image: "./sportsTube.gif",
+    link: "https://sportstube.netlify.app/", 
+  },
+  {
+    title: "Supplies Management System",
+    subtitle: "Oracle SQL, Oracle PL/SQL, HTML5, and JavaScript",
+    description:
+      "Created a full stack application using Oracle Apex.",
+    image: "./sms.gif",
+    link: "https://apex.oracle.com/pls/apex/r/oracleapextraining2024/supplies-management-system/login", 
+  },
+  {
+    title: "Dashboard",
+    subtitle: "Chart.js, HTML5, Tailwind CSS, Bootstrap, jQuery, JSP, and Tomcat Server ",
+    description:
+      "Created a full stack application using Oracle Apex.",
+    image: "./dashBoard.gif",
+    link: null, 
+  },
+  {
     title: "Todo List App",
-    subtitle: "React",
+    subtitle: "React and Bootstrap",
     description:
       "Created a todo list app using React.js and was deployed to github pages.",
     image: "./TODOs.gif",
@@ -14,11 +45,11 @@ export const projects = [
       "Created a weather with an API to get temperature, humidity, and wind speed to a certain location.",
     image: "./weather.gif",
     link: "https://weather-app-sendoh.netlify.app/",
-  },
+  }
 ];
 
 export const skills = [
-  "JavaScript",
+  "HTML5/ CSS3/ JavaScript",
   "React.js",
   "Node.js",
   "Java",
