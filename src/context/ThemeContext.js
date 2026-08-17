c4f694c5148a0c8fadfe9fe7338b05b35f5c372f
jrenz <johnrenz@thinkbitsolutions.com>
2026-08-17 21:14:05 +0800

    dark default

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,7 +8,7 @@ export function useTheme() {
 }
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
